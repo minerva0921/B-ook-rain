c3b1b287fb128b88d305c2628406a7c0de7ff991
minerva0921 <121062hk@gmail.com>
2022-02-11 17:39:12 +0000

    [middle0212] search

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,12 @@
 class Book < ApplicationRecord
+  
+  
+  def self.search(search)
+    if search
+      where(['title LIKE ?', "%#{search}%"]) #titleの部分一致を表示
+    else
+      all #全て表示
+    end
+  end
+
 end
